03b58cfd3ae966e5b66da415e71999e3a9cbee81
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2020-02-12 19:45:47 +0900

    finish QRcode

--- Customer Application/app/src/main/java/com/example/customerapplication/OpenDoorLock.java
+++ Customer Application/app/src/main/java/com/example/customerapplication/OpenDoorLock.java
@@ -6,19 +6,43 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcManager;
 import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
 public class OpenDoorLock extends AppCompatActivity {
 
+    private IntentIntegrator qrScan;
+    private ImageView iv;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.open_door_lock);
+
+
+        iv = (ImageView)findViewById(R.id.qrcode);
+        text = "0x459451d1229fa9d8d02c97b2352408465a293158";
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try{
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            iv.setImageBitmap(bitmap);
+        }catch (Exception e){}
     }
+
 }
