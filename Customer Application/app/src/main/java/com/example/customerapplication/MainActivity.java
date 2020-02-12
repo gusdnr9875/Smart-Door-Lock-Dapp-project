1675886d89900f1646d5c9763beadddbe33a3e74
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2020-02-12 13:29:51 +0900

    add start activity

--- Customer Application/app/src/main/java/com/example/customerapplication/MainActivity.java
+++ Customer Application/app/src/main/java/com/example/customerapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.customerapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void started(View view){
+        Toast.makeText(getApplicationContext(),"버튼이 눌러졌습니다.",Toast.LENGTH_SHORT).show();
+        Intent in = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(in);
+
     }
 }
