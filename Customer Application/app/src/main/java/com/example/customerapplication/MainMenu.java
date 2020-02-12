a76b354ddba7475b2eb1601a9eff9e2dd3407f1b
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2020-02-12 14:04:23 +0900

    connect reservation activity

--- Customer Application/app/src/main/java/com/example/customerapplication/MainMenu.java
+++ Customer Application/app/src/main/java/com/example/customerapplication/MainMenu.java
@@ -2,7 +2,9 @@ package com.example.customerapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_main);
+    }
+
+    public void reservate(View view){
+        Intent in = new Intent(MainMenu.this, ReservateHotel.class);
+        startActivity(in);
     }
 
 
