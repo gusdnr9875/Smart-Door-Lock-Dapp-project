849817aa4958cbc694f32ff5ee51127007be1494
Minsub Kim <dev.suub@gmail.com>
2020-02-14 06:11:20 +0900

    end2?

--- Customer Application/app/src/main/java/com/example/customerapplication/presenter/WriteFeedPresenter.java
+++ Customer Application/app/src/main/java/com/example/customerapplication/presenter/WriteFeedPresenter.java
@@ -4,6 +4,8 @@ package com.example.customerapplication.presenter;
 import android.text.TextUtils;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
 import com.example.customerapplication.OpenDoorLock;
 import com.samsung.android.sdk.coldwallet.ScwCoinType;
 import com.samsung.android.sdk.coldwallet.ScwService;
@@ -12,7 +14,13 @@ import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Bool;
 import org.web3j.abi.datatypes.Function;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
+import java.math.BigInteger;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,7 +29,7 @@ import static java.util.Collections.singletonList;
 
 public class WriteFeedPresenter {
     public static String address;
-
+    public static String signedTxHex;
     public static final String CONTRACT_ADDRESS = "0xbFa162ba4C12a2C4F94ed0e5de3495A9A3aBBCb3";
 
     public static Function createGetPostCountSmartContractCall() {
@@ -39,7 +47,58 @@ public class WriteFeedPresenter {
         return data;
     }
 
+    public static void sign() {
+        try {
 
+            String hdPath = ScwService.getHdPath(ScwCoinType.ETH, 0);
+
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+
+            RawTransaction unsignedTransaction = RawTransaction.createTransaction(new BigInteger("10"), Convert.toWei("400", Convert.Unit.GWEI).toBigInteger()
+                    , new BigInteger("41000"), CONTRACT_ADDRESS, Long.toString(timestamp.getTime()));
+
+
+            byte[] byteArr = TransactionEncoder.encode(unsignedTransaction);
+
+            ScwService.getInstance().signEthTransaction(new ScwService.ScwSignEthTransactionCallback() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    signedTxHex = Numeric.toHexString(bytes);
+                    Log.i("rrr",signedTxHex);
+
+                }
+                @Override
+                public void onFailure(int i, @Nullable String s) {
+                    Log.e("signEth", s);
+                }
+            }, byteArr,hdPath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ScwService.ScwGetAddressListCallback callback =
+                new ScwService.ScwGetAddressListCallback() {
+                    @Override
+                    public void onSuccess(List<String> addressList) {
+                        Log.i("hello", addressList.get(0)); //public address
+                        address = addressList.get(0);
+                        Log.i("qqq",address);
+                    }
+
+                    @Override
+                    public void onFailure(int errorCode, String errorMessage) {
+                        //handle errors
+
+                    }
+                };
+
+        String hdPath = ScwService.getHdPath(ScwCoinType.ETH, 0);
+        ArrayList<String> hdPathList = new ArrayList<>();
+        hdPathList.add(hdPath);
+        ScwService.getInstance().getAddressList(callback, hdPathList);
+    }
     public static void getEthereumAddress() {
         ScwService.ScwGetAddressListCallback callback =
                 new ScwService.ScwGetAddressListCallback() {
