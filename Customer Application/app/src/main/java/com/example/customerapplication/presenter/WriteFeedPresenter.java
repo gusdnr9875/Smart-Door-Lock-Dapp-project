20006071182ae1f6192038c83b15635ad8e78784
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2020-02-13 12:23:30 +0900

    Adsas

---
+++ Customer Application/app/src/main/java/com/example/customerapplication/presenter/WriteFeedPresenter.java
@@ -0,0 +1,10 @@
+package com.example.customerapplication.presenter;
+import android.support.annotation.Nullable;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+public class WriteFeedPresenter {
+
+
+}
