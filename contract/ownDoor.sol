01f20f2ee933f8d3f007da5f20f8f029ce4035a7
Minsub Kim <dev.suub@gmail.com>
2020-02-13 11:33:45 +0900

    Update opendoor function

--- contract/ownDoor.sol
+++ contract/ownDoor.sol
@@ -1,48 +1,52 @@
 pragma solidity >=0.4.22 <0.7.0;
 
-// TODO
-// 1. 도어락 여러개 관리할수있도록
-// 2. 테스트
-// 3. 타임스탬프 확인할수있도록 추가
-
 contract OwnDoor {
+    
+    struct BookingInfo {
+        bool isBooked;
+        uint startTime;
+        uint endTime;
+    }
     
     struct Door {
         uint id;
         address owner;
-        mapping (address => bool) booked;
+        mapping (address => BookingInfo) bookingInfo;
     }
-    
+
     Door public door;
 
-    constructor() {
-    
+    constructor() public {
+        door.id = 1;
+        door.owner = msg.sender;
     }
     
-    // validate booking
-    modifier validateGuest(address guest) {
-        require(door.booked[guest] == true);
-        _;
-    }
-    function openDoor(uint id)  validateGuest(msg.sender) public view returns(bool) {
-        require(door.id == id);
-        return true
+    function openDoor() public view returns(bool) {
+        // require(door.id == id);
+        if (door.bookingInfo[msg.sender].isBooked == true) {
+            if (now < door.bookingInfo[msg.sender].endTime) {
+                if (now > door.bookingInfo[msg.sender].startTime) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     
-    
-    // CRUD booking
+    // CURD booking
     modifier isOwner(address owner) {
         require(door.owner == owner);
         _;
     }
-    function addBooking(address guest) isOwner(msg.sender) public view returns(bool) {
-        door.booked[guest] = true;
+    function addBooking(address guest, uint startTime, uint endTime) isOwner(msg.sender) public  returns(bool) {
+        door.bookingInfo[guest].isBooked = true;
+        door.bookingInfo[guest].startTime = startTime;
+        door.bookingInfo[guest].endTime = endTime;
         return true;
     }
     
-    function removeBooking(address guest) isOwner(msg.sender) public view returns(bool) {
-        door.booked[guest] = false;
+    function removeBooking(address guest) isOwner(msg.sender) public  returns(bool) {
+        door.bookingInfo[guest].isBooked = false;
         return true;
     }    
-    
 }
